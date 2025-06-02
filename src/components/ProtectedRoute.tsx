023f059d82282e53fd71d7f48f23d375fb8dbc98
Bhupesh Khandelwal <bhupeshkhandelwal@Bhupeshs-MacBook-Pro.local>
2025-06-02 21:21:13 +0530

    Build Issues Resolved

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {

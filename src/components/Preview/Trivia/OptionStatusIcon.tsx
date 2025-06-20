3d88a4e25d85a40a45c3941b9a8f3ad425074e53
Paras Kumar <paras.kumar@almond.solutions>
2025-06-20 10:58:25 +0530

    preview fixed real questions not showing

--- src/components/Preview/Trivia/OptionStatusIcon.tsx
+++ src/components/Preview/Trivia/OptionStatusIcon.tsx
@@ -8,8 +8,8 @@ export function OptionStatusIcon({
       <img
         src={
           state === "incorrect"
-            ? "/assets/incorrect_icon.png?v=1"
-            : "/assets/incorrect_icon.png?v=1"
+            ? "/images/icon/incorrect_icon.png?v=1"
+            : "/images/icon/correct_icon.png?v=1"
         }
         alt=""
         className="h-[20px] w-[20px]"
